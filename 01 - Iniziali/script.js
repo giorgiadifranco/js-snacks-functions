b8a000660554b0ba82c5ba94ff28e88c452ae03e
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-07 14:41:53 +0200

    Add map methds

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,37 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+let  nomi = names.map(myFunction);
+
+//let lunghezzaNomi = nomi.length
+
+/*const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+function myFunction(value, index, array) {
+  return value * 2;
+}*/
+
+
+for (i = 0; 0 > lunghezzaNomi; i--){
+        //let name = names.length[i];
+        //console.log(name);
+
+
+    }
+        
+
 
 // Dichiara la funzione qui.
+/*functions initialLetter (array){
 
+    
+
+}
+initialLetter()
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
 
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]*/
