9d2e15400aecf80ff75fef57ff229b19a2fc43aa
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-07 17:05:11 +0200

    add array A letter

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,30 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const letter = "A";
+
+
+
 
 
 // Dichiara la funzione qui.
 
 
+function filterByLetter(words, letter) {
+    const filteredWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].charAt(0).toLowerCase() === letter.toLowerCase()) {
+            filteredWords.push(words[i]);
+        }
+    }
+
+    return filteredWords;
+}
+
+
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filterByLetter(names, letter)); 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
